1a860a2b1d711172bdaa9953bf2d3b82dd128995
Aji Setiawan <106381048+ajisetiawan716@users.noreply.github.com>
2023-09-11 11:02:45 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+	// Ganti 'YOUR_API_KEY' dengan kunci API Google Drive Anda
+	$apiKey = "YOUR_API_KEY";
+
+    // Ganti 'YOUR_API_KEY' dengan kunci API Google Sheets Anda
+    $Gsheet_apiKey = "YOUR_API_KEY";
+	
+	// Mengatur URL Aria2 RPC
+	$aria2RpcUrl = 'http://localhost:6800/jsonrpc';
+
+	// Mengatur username dan password Aria2 RPC
+	$aria2Username = 'your_username';
+	$aria2Password = 'your_password';
+
+?>
